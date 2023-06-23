e9ce0c8f31a6056a6dbbb3aa70d903bcd9c88dc2
Mohamed tefili <m.ecomplus1@gmail.com>
2023-06-23 16:03:18 +0200

    Ajout des filtres dans la gallerie

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -37,9 +37,78 @@ function fetchWorksDisplayGallery(targetElement) {
 })
 }
 
-  // Récupère les données de l'API Works
-  fetchWorks()
-  .then(dataWork => {
-// Affiche le projet dans la gallerie
-fetchWorksDisplayGallery(".gallery");
-  })
+/****************************FILTRES DE LA GALLERIE******************************************/
+
+
+
+  // Récupérer tous les éléments nécessaires
+const galleryElement = document.querySelector(".gallery");
+const filterObjetsButton = document.getElementById("filter-objets");
+const filterAppartementsButton = document.getElementById("filter-appartements");
+const filterHotelsRestaurantsButton = document.getElementById("filter-hotels-restaurants");
+const filterTousButton = document.getElementById("filter-tous");
+
+// Variable pour stocker les données des projets
+let projectsData = [];
+
+// Fonction pour récupérer les projets via l'API
+function fetchWorks() {
+  return fetch("http://localhost:5678/api/works")
+    .then(response => response.json())
+    .then(data => {
+      projectsData = data; // Stocker les données des projets
+      return data;
+    })
+    .catch(error => {
+      console.log("Erreur lors de la récupération des projets :", error);
+    });
+}
+
+
+// Fonction pour afficher les projets sur la page d'accueil
+function displayProjectsInGallery(filter = null) {
+  galleryElement.innerHTML = ''; // Effacer le contenu de la galerie
+
+  projectsData.forEach(jsonWork => {
+    if (filter && jsonWork.category.name !== filter) {
+      return; // Ignorer les projets qui ne correspondent pas au filtre
+    }
+
+    const figure = document.createElement('figure');
+    figure.classList.add('work');
+    figure.dataset.category = jsonWork.categoryId;
+
+    const img = document.createElement('img');
+    img.src = jsonWork.imageUrl;
+    img.alt = 'image du projet';
+    figure.appendChild(img);
+
+    const figcaption = document.createElement('figcaption');
+    figcaption.textContent = jsonWork.title;
+    figure.appendChild(figcaption);
+
+    galleryElement.appendChild(figure);
+  });
+}
+
+// Récupérer les données des projets et afficher tous les projets par défaut
+fetchWorks().then(() => {
+  displayProjectsInGallery();
+});
+
+// Écouteurs d'événements pour les boutons de filtre
+filterTousButton.addEventListener("click", () => {
+  displayProjectsInGallery();
+});
+
+filterObjetsButton.addEventListener("click", () => {
+  displayProjectsInGallery("Objets");
+});
+
+filterAppartementsButton.addEventListener("click", () => {
+  displayProjectsInGallery("Appartements");
+});
+
+filterHotelsRestaurantsButton.addEventListener("click", () => {
+  displayProjectsInGallery("Hotels & restaurants");
+});
